bad555ebeb5bb1be321a839cde4afabfdb02e525
Arkady <arkad0912@mail.ru>
2025-05-15 09:41:40 +0300

    Исправление в дз

--- internal/pages/handler.go
+++ internal/pages/handler.go
@@ -2,30 +2,33 @@ package pages
 
 import "github.com/gofiber/fiber/v2"
 
-type PagesHandler struct {
-	router fiber.Router
-}
+// type PagesHandler struct {
+// 	router fiber.Router
+// }
 
-//1
-func NewPagesHandler(router fiber.Router) *PagesHandler {
-	h := &PagesHandler{
-		router: router,
-	}
+// //1
+// func NewPagesHandler(router fiber.Router) *PagesHandler {
+// 	h := &PagesHandler{
+// 		router: router,
+// 	}
 
-	h.registerRoutes()
-	return h
-}
+// 	h.registerRoutes()
+// 	return h
+// }
 
-func (h *PagesHandler) registerRoutes() {
+// func (h *PagesHandler) registerRoutes() {
 
-	api := h.router.Group("/api")
+// 	api := h.router.Group("/api")
 
-	api.Get("/", h.HomePagesHandler)
+// 	api.Get("/", h.HomePagesHandler)
 
-}
+// }
 
-func (h *PagesHandler) HomePagesHandler(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"message": "Home pages",
-	})
+// func (h *PagesHandler) HomePagesHandler(c *fiber.Ctx) error {
+// 	return c.JSON(fiber.Map{
+// 		"message": "Home pages",
+// 	})
+// }
+func Handler(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
 }
