353fb53679ef8cc1d21c311e17c63dc474316b31
Arkady <arkad0912@mail.ru>
2025-05-16 15:48:09 +0300

    2-logger

--- cmd/main.go
+++ cmd/main.go
@@ -4,23 +4,27 @@ import (
 	"dz/config"
 	"dz/internal/pages"
 	"log"
+	"log/slog"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	slogfiber "github.com/samber/slog-fiber"
 )
 
 func main() {
 	//инициализация конфига
 	config.Init()
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	dbConf := config.NewDatabaseConfig()
 	log.Println(dbConf)
 
-	//Создание Fiber
+	// Создание Fiber приложения
 	app := fiber.New()
 
-	//Инициализация
-
-	app.Use(recover.New()) //мидлвейр, обрабатывает ошибки
+	// Подключение middleware
+	app.Use(recover.New())         // Мидлвейр, обрабатывающий ошибки
+	app.Use(slogfiber.New(logger)) // Логирование запросов
 
 	//pages.NewPagesHandler(app)
 	app.Get("/", pages.Handler) // Обработчик для корневого пути
